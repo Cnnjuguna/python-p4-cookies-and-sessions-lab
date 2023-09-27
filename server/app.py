f1c61f31219d09dd2fa4d19d6a3a2d67a61732f4
Cnnjuguna <123736824+Cnnjuguna@users.noreply.github.com>
2023-09-27 14:15:48 +0300

    Completed assignment

--- server/app.py
+++ server/app.py
@@ -1,34 +1,53 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response, jsonify, session
+from pickle import TRUE
+from urllib import response
+from flask import Flask, make_response, jsonify, request, session
 from flask_migrate import Migrate
 
 from models import db, Article, User
 
 app = Flask(__name__)
-app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = b"Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/clear')
+
+@app.route("/clear")
 def clear_session():
-    session['page_views'] = 0
-    return {'message': '200: Successfully cleared session data.'}, 200
+    session["page_views"] = 0
+    return {"message": "200: Successfully cleared session data."}, 200
 
-@app.route('/articles')
+
+@app.route("/articles")
 def index_articles():
+    articles_dict = [article.to_dict() for article in Article.query.all()]
+    response = make_response(jsonify(articles_dict), 200)
+    response.headers["Content-Type"] = "application/json"
+    return response
 
-    pass
 
-@app.route('/articles/<int:id>')
+@app.route("/articles/<int:id>", methods=["GET"])
 def show_article(id):
+    if request.method == "GET":
+        session["page_views"] = session.get("page_views", 0)
+        session["page_views"] += 1
 
-    pass
+        article = Article.query.filter_by(id=id).first()
+        if session["page_views"] <= 3:
+            response_dict = article.to_dict()
+            response = make_response(jsonify(response_dict), 200)
+            return response
+        else:
+            response_body = {"message": "Maximum pageview limit reached"}
+            response = make_response(jsonify(response_body), 401)
+            return response
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(port=5555)
